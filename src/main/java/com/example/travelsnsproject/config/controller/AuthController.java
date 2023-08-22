ab429abd94c01eac9767053bfaf6b586b249db02
dokyeong99 <dlehrud451@gmail.com>
2023-08-22 15:28:37 +0900

    a

--- src/main/java/com/example/travelsnsproject/config/controller/AuthController.java
+++ src/main/java/com/example/travelsnsproject/config/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("api/v1/auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final OauthService oauthService;
